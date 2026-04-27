394d5897021c0a8ed02089c5781e54064b2ce471
Juan David Sanchez Loaiza <juan.sanchezl@agencianacionaldigital.com.co>
2026-04-27 17:07:53 -0500

    Subida inicial

--- fna-template-ui.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="fna-template-ui" />
-export * from './public-api';
--- public-api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/fna-template-ui.service';
-export * from './lib/fna-template-ui.component';
-export * from './lib/fna-template-ui.module';
-export * from './lib/header/header.component';
-export * from './lib/footer/footer.component';
